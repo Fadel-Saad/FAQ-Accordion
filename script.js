0d6437cb2264285a45e8332b41b8e5f5cf5b02cd
Fadel-Saad <fadelsaad.dev@gmail.com>
2024-09-19 23:52:58 +0300

    fix grid ratio when button is clicked + add JS documentation

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const buttons = $(".btn");
-const answer = $(".answer");
+const questionText = $(".q-text");
 
-buttons.on("click", clicked);
+buttons.on("click", showAnswer);
 
-function clicked()  {
-  $(this).next("h3").toggle("hidden");
+function showAnswer()  {
+  $(this).next(".answer").toggle("hidden"); // shows the answer
 
-  let currentSrc = $(this).find("img").attr("src");
+  let currentSrc = $(this).find("img").attr("src"); // selects current img source
   
-  if (currentSrc === "./assets/images/icon-plus.svg") {
-    $(this).find("img").attr("src", "./assets/images/icon-minus.svg");
-  } else  {
+  if (currentSrc === "./assets/images/icon-plus.svg") { // if the icon is plus sign
+    $(this).find("img").attr("src", "./assets/images/icon-minus.svg"); 
+    // ^ select img element in the button clicked to change its svg
+    $(this).parent().css("grid-template-rows", "1fr 3fr");
+    // ^ select the parent div to change the grid rows ratio
+    $(this).parent().css("margin-bottom", "0");
+    $(this).next(".answer").css("align-content", "end");
+  } else  { // if icon is not plus (minus)
     $(this).find("img").attr("src", "./assets/images/icon-plus.svg");
+    $(this).parent().css("grid-template-rows", "1fr 1fr");
+    $(this).parent().css("margin-bottom", "20px");
   }
+  
 }
